2800d404d14d36f6afeb8233f9426f2e375b35fd
mr_destructive <gormeet711@gmail.com>
2023-08-18 18:51:32 +0530

    add health status apis

--- health.go
+++ health.go
@@ -9,6 +9,16 @@ type HealthStatus struct {
 	Status string `json:"status"`
 }
 
+type HealthQueue struct {
+	Size int `json:"size"`
+}
+
+type HealthTime struct {
+	RealTime  int `json:"realTime"`
+	LocalTime int `json:"localTime"`
+	Diff      int `json:"diff"`
+}
+
 func (srv *Client) Health() (*HealthStatus, error) {
 	path := "/health"
 	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
@@ -35,4 +45,80 @@ func (srv *Client) DBHealth() (*HealthStatus, error) {
 	return &result, nil
 }
 
+func (srv *Client) CacheHealth() (*HealthStatus, error) {
+	path := "/health/cache"
+	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result HealthStatus
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
 
+func (srv *Client) LocalStorageHealth() (*HealthStatus, error) {
+	path := "/health/storage/local"
+	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result HealthStatus
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (srv *Client) FunctionsQueue() (*HealthQueue, error) {
+	path := "/health/queue/functions"
+	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result HealthQueue
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (srv *Client) LogsQueue() (*HealthQueue, error) {
+	path := "/health/queue/logs"
+	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result HealthQueue
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (srv *Client) WebHooksQueue() (*HealthQueue, error) {
+	path := "/health/queue/webhooks"
+	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result HealthQueue
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (srv *Client) TimeHealth() (*HealthTime, error) {
+	path := "/health/time"
+	resp, err := srv.CallAPI("GET", path, srv.headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result HealthTime
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
